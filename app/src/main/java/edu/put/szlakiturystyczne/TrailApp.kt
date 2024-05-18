9635fca899c1750486ef6bc81846f32e381db96d
kubajarmuzek <102328573+kubajarmuzek@users.noreply.github.com>
2024-05-18 14:34:54 +0200

    basic navigation

--- app/src/main/java/edu/put/szlakiturystyczne/TrailApp.kt
+++ app/src/main/java/edu/put/szlakiturystyczne/TrailApp.kt
@@ -6,34 +6,78 @@ import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
 
 @Composable
 fun TrailApp() {
+    val navController = rememberNavController()
     val trails = remember { trails }
     val selectedTrail = remember { mutableStateOf<Trail?>(null) }
 
-    Surface(color = MaterialTheme.colors.background) {
-        if (isWideScreen()) {
-            Row(modifier = Modifier.fillMaxSize()) {
-                Column(modifier = Modifier.weight(1f)) {
-                    TrailList(trails) { trail -> selectedTrail.value = trail }
+
+
+    Scaffold(
+        bottomBar = {
+            BottomNavigation {
+                BottomNavigationItem(
+                    selected = navController.currentDestination?.route == "home",
+                    onClick = {
+                        navController.navigate("home") {
+                            launchSingleTop = true
+                        }
+                    },
+                    icon = { /* Home icon */ },
+                    label = { Text("Home") }
+                )
+                BottomNavigationItem(
+                    selected = navController.currentDestination?.route == "details",
+                    onClick = {
+                        navController.navigate("details") {
+                            launchSingleTop = true
+                        }
+                    },
+                    icon = { /* Details icon */ },
+                    label = { Text("Details") }
+                )
+            }
+        }
+    ) { padding ->
+        Column(
+            modifier = Modifier.padding(padding)
+        )
+        {
+            NavHost(navController = navController, startDestination = "home") {
+                composable("home") {
+                    HomeScreen(navController = navController)
                 }
-                selectedTrail.value?.let { trail ->
-                    Column(modifier = Modifier.weight(1f)) {
-                        TrailDetails(trail) {
-                            selectedTrail.value = null
+                composable("details") {
+                    if (isWideScreen()) {
+                        Row(modifier = Modifier.fillMaxSize()) {
+                            Column(modifier = Modifier.weight(1f)) {
+                                TrailList(trails) { trail -> selectedTrail.value = trail }
+                            }
+                            selectedTrail.value?.let { trail ->
+                                Column(modifier = Modifier.weight(1f)) {
+                                    TrailDetails(trail) {
+                                        selectedTrail.value = null
+                                    }
+                                }
+                            }
+                        }
+                    } else {
+                        if (selectedTrail.value == null) {
+                            TrailList(trails) { trail -> selectedTrail.value = trail }
+                        } else {
+                            TrailDetails(selectedTrail.value!!) {
+                                selectedTrail.value = null
+                            }
                         }
                     }
                 }
             }
-        } else {
-            if (selectedTrail.value == null) {
-                TrailList(trails) { trail -> selectedTrail.value = trail }
-            } else {
-                TrailDetails(selectedTrail.value!!) {
-                    selectedTrail.value = null
-                }
-            }
         }
-    }
-}
+
+    }}
